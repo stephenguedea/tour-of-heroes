57cf178960cf2dfc4b09313eba730dc11e47784f
Stephen Ray Guedea <sguedea@appddictionstudio.com>
2021-06-22 10:55:54 -0500

    in progress of getting data from a server

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,28 +1,91 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, pipe } from 'rxjs';
 import { HEROES } from '../data/mock-heroes';
 import { Hero } from '../models/Hero';
 import { MessageService } from './message.service';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes'; // URL to web api
+  // defining the heroesUrl of the form ':base/:collectionName' with the address of the heroes
+  // resource on the server. Here 'base' is the resource to which requests are made, and 
+  // 'collectionName' is the heroes data object in the 'in-memory-data-service.ts'
 
-  constructor(private messageSvc: MessageService) { }
+  constructor(private messageSvc: MessageService,
+              private http: HttpClient) { }
 
-  // service method for components for retrieve data
+
+  // new service method
+  // GET heroes from the server
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageSvc.add("HeroService: fetched heroes");
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+      // the catchError() operator intercepts an Observable that failed.
+      // The operator then passes the error to the error handling function
+    );
   }
 
+  // updated getHero() method
+  // GET hero by id. Will 404 if id not found
   getHero(id: number): Observable<Hero> {
-    // For now, assume that a hero with the specified `id` always exists.
-    // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageSvc.add(`Hero Service: fetched hero id = ${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id = ${id}`)),
+      catchError(this.handleError<Hero>(`getHero id =${id}`))
+    );
   }
+
+  // new getHero() method:
+  /*
+  - getHero() constructs a request URL with the desired hero's id
+  - the server should respond with a single hero rather than an array of heroes
+  - getHero() returns an Observable<Hero> ("an observable of Hero objects") rather 
+  than an observable of hero arrays.
+  */
+
+  // Log a HeroService message with the MessageService
+  private log(message: string) {
+    this.messageSvc.add(`HeroService: ${ message }`);
+  }
+
+
+  // handleError() method reports the error and then returns as innocuous result so that
+  // the application keeps working
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning any empty result
+      return of(result as T);
+
+    };
+  }
+
+  // the handleError() will be shared by many 'HeroService' methods so it's generalized to meet
+  // their different needs.
+
+  /*
+  Instead of handling the error directly, it returns an error handler function to 'catchError'
+  that it has configured with both the name of the operation that failed and a safe return value
+  */
+  
 }
+
+/*
+After reporting the error to the console, the handler constructs a user friendly 
+message and returns a safe value to the application so the application can keep working.
+
+Because each service method returns a different kind of Observable result, 
+'handleError()' takes a type parameter so it can return the safe value as the type
+that the application expects.
+*/
---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from '../models/Hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Dr. Nice'},
+      { id: 12, name: 'Narco'},
+      { id: 13, name: 'Bombasto'},
+      { id: 14, name: 'Celeritas'},
+      { id: 15, name: 'Magneta'},
+      { id: 16, name: 'RubberMan'},
+      { id: 17, name: 'Dynama'},
+      { id: 18, name: 'Dr IQ'},
+      { id: 19, name: 'Magma'},
+      { id: 20, name: 'Tornado'},
+    ]
+    return {heroes};
+  }
+
+  constructor() { }
+
+  /* Overrides the genId method to ensure that a hero always has an id.
+  If the heroes array is empty, the method below returns the initial number (11).
+  If the heroes array is not empty, the method below returns the highest 
+  hero id + 1.
+  */
+ genId(heroes: Hero[]): number {
+   return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+ }
+
+ // this particular get() call returns an Observable<Hero[]>; that is, "an observable of hero arrays"
+ // in this practice, it will only return a single hero array
+ 
+}
+
+// This file will take over the function of mock-heroes.ts. However,
+// don't delete mock-heroes.ts yet, still need it...
