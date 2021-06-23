76b4e5b9bdb8b08beeacfa09351f899af30d2b23
Stephen Ray Guedea <sguedea@appddictionstudio.com>
2021-06-23 10:58:37 -0500

    Tour of Heroes App completed

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -19,6 +19,7 @@ export class HeroDetailComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
   getHero() {
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -52,6 +52,20 @@ export class HeroService {
   than an observable of hero arrays.
   */
 
+   /** GET hero by id. Return `undefined` when id not found */
+   getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url)
+      .pipe(
+        map(heroes => heroes[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? `fetched` : `did not find`;
+          this.log(`${outcome} hero id=${id}`);
+        }),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
+
   // Log a HeroService message with the MessageService
   private log(message: string) {
     this.messageSvc.add(`HeroService: ${ message }`);
@@ -114,6 +128,31 @@ export class HeroService {
     catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+
+  /* 
+  deleteHero() calls HttpClient.delete()
+  the url is the heroes resource url plus the id of the hero to delete
+  you don't send data as you did with put() and post()
+  you still send the httpOptions
+  */
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap( x => x.length ?
+          this.log(`found heroes matching "${term}"`) :
+          this.log(`no heroes matching "${term}"`)),
+          catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+  /* The method returns immediately with an empty array if there is no search term.
+     The rest of it closely resembles getHeroes(), the only significant difference being the URL,
+     which includes a query string with the search term
+  */
   
 }
 
